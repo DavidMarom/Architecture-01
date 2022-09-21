0800797bdb1f9a816ac20a9039004aeb1eb81dbb
David Marom <davidmarom@hotmail.com>
2022-09-21 13:52:06 +0300

    update

--- src/components/PageComp02/PageComp02.jsx
+++ src/components/PageComp02/PageComp02.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { PageContainer } from './PageComp.styles'
 import { db } from '../../firebase-config'
-import { collection, getDocs, addDoc } from 'firebase/firestore'
+import { collection, getDocs, addDoc, updateDoc, doc } from 'firebase/firestore'
 import { useSelector } from 'react-redux'
 
 export const PageComp02 = () => {
@@ -15,7 +15,12 @@ export const PageComp02 = () => {
             item01: newItem
         })
         setNewItem("")
+    }
 
+    const updateItem = async (id) => {
+        const itemDoc = doc(db, "list", id)
+        const newFields = { item01: 'aaaaaaaaaa' }
+        await updateDoc(itemDoc, newFields);
     }
 
     useEffect(() => {
@@ -28,12 +33,21 @@ export const PageComp02 = () => {
 
     return (
         <PageContainer darkMode={isDark} >
+            {console.log(items)}
             <h1>Page 02</h1>
 
             <input type="text" onChange={(ev) => { setNewItem(ev.target.value) }} />
             <button onClick={createNew}>Add</button>
 
-            {items.map((item, idx) => <h1 key={idx}>{item.item01}</h1>)}
-        </PageContainer>
+
+            {items.map((item, idx) => {
+                return <div key={idx}>
+                    <p>{item.item01}</p>
+                    <button onClick={() => { updateItem(item.id) }}>Update</button>
+                </div>
+            })}
+
+
+        </PageContainer >
     )
 }
