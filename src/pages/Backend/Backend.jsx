8e1bace95e9ba79165ffdcf5179aa0aae1afbf8d
David Marom <davidmarom@hotmail.com>
2022-10-11 14:11:05 +0300

    Added diagram

--- src/pages/Backend/Backend.jsx
+++ src/pages/Backend/Backend.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Col, PageContainer } from './Backend.styles'
 
 
 export const Backend = () => {
     const isDark = useSelector(state => state.settings.dark)
-
-    const callAPI = () => {
-        const res = fetch('https://davidmaromapi.com/api/files/add')
+    const [name, setName] = useState('')
+    const callAPI = (name) => {
+        const res = fetch('https://davidmaromapi.com/api/files/add?name=' + name)
         res.then(res => res.json())
             .then(res => window.open('https://davidmaromapi.com/zip/' + res.file))
-
-                
     }
 
+    return (
+        <PageContainer darkMode={isDark} >
+            <Col>
+                <h2>Backend Demonstration</h2>
+                <p>Host: Digital Ocean {`(`}Ubuntu{`)`}</p>
+                <p>Server: nginx + systemd</p>
+                <p>Process Manager: pm2</p>
+                <p>API: <span><a href="https://davidmaromapi.com" target="_blank" rel="noreferrer">https://davidmaromapi.com</a></span></p>
+                <p></p>
+                <p><b>Demonstration 01:</b></p>
+                <p>Enter your name, the server will append it to template.txt, and will send it to you in Your_File.zip</p>
+                <p></p>
+                <input type="text" id="name" placeholder="Enter a name" onChange={(ev) => { setName(ev.target.value) }} />
+                <button onClick={() => callAPI(name)}>Download file</button>
+                <p></p>
+                <img src="infrastructure.png" alt="infrastructure" width="500px" />
 
-return (
-    <PageContainer darkMode={isDark} >
-        <Col>
-            <h2>Backend Demonstration</h2>
-            <p>Host: Digital Ocean {`(`}Ubuntu{`)`}</p>
-            <p>Server: nginx + systemd</p>
-            <p>Process Manager: pm2</p>
-            <p>API: <span><a href="https://davidmaromapi.com" target="_blank" rel="noreferrer">https://davidmaromapi.com</a></span></p>
-
-            <button onClick={callAPI}>Download file</button>
-
-        </Col>
-    </PageContainer>
-)
+            </Col>
+        </PageContainer>
+    )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,14 +16,18 @@ export const Home = () => {
                 <li>React 18 updates.</li>
                 <p></p>
                 <p><b>It also uses</b> Firebase for authentication, Axios, Styled Components, AntD, Formik etc...</p>
-                <p>Git: <a href="https://github.com/DavidMarom/Architecture-01" target="_blank" rel="noopener">https://github.com/DavidMarom/Architecture-01</a></p>
+                <p>Git: <a href="https://github.com/DavidMarom/Architecture-01" target="_blank" rel="noopener noreferrer">https://github.com/DavidMarom/Architecture-01</a></p>
                 <p></p>
                 <p>Make sure to <Link to="/login">login</Link> in order to see all the inner pages and demonstrations</p>
                 <p></p>
                 <h3><b>For UI-oriented projects:</b></h3>
-                <a href="https://health-e.herokuapp.com/#/" target="_blank" rel="noopener">Health-e</a>
-                <a href="https://clock-in-demo.herokuapp.com/" target="_blank" rel="noopener">Clock-In Demo</a>
-                <a href="https://www.karmanow.com/" target="_blank" rel="noopener">Karma Shopping</a>
+                <a href="https://health-e.herokuapp.com/#/" target="_blank" rel="noopener noreferrer">Health-e</a>
+                <a href="https://clock-in-demo.herokuapp.com/" target="_blank" rel="noopener noreferrer">Clock-In Demo</a>
+                <a href="https://www.karmanow.com/" target="_blank" rel="noopener noreferrer">Karma Shopping</a>
+                <p></p>
+                <a href="https://davidmarom.com/infrafstructure.png" target="_blank" rel="noopener noreferrer">
+                    <img src="infrastructure.png" alt="infrastructure" width="500px" />
+                </a>
             </Col>
         </PageContainer>
     )
