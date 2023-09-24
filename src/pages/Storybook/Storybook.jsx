145f80901e9d0177fce116aa4e630f4f2a43671c
David Marom <davidmarom.pro@gmail.com>
2023-09-24 20:22:38 +0300

    updated storybook

--- src/pages/Storybook/Storybook.jsx
+++ src/pages/Storybook/Storybook.jsx
@@ -9,7 +9,8 @@ const Storybook = () => {
 
         <PageContainer darkMode={isDark} >
             <h1>Storybook</h1>
-            <a href="https://6346c29f12cb3212588eb719-inmayifyhi.chromatic.com/?path=/story/welcome--home" target="_blank" rel="noreferrer">Link to my Storybook (UI design system)</a>
+            {/* <a href="https://6346c29f12cb3212588eb719-inmayifyhi.chromatic.com/?path=/story/welcome--home" target="_blank" rel="noreferrer">Link to my Storybook (UI design system)</a> */}
+            <a href="https://6346c29f12cb3212588eb719-kgsdjqityn.chromatic.com/?path=/story/welcome--home" target="_blank" rel="noreferrer">Link to my Storybook (UI design system)</a>
         </PageContainer>
     )
 }
