791239d0e1a548b7ea2a32dcaa100d71a9b130b1
David Marom <davidmarom.pro@gmail.com>
2024-06-02 13:23:39 +0300

    removed antD

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -19,7 +19,7 @@ const TopNav=()=> {
             </Col>
             <Row>
                 <NavLink to="/" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>Home</p></NavLink>
-                <NavLink to="/antd" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>AntD</p></NavLink>
+                {/* <NavLink to="/antd" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>AntD</p></NavLink> */}
                 {/* <NavLink to="/form" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>Form</p></NavLink> */}
                 {/* <NavLink to="/FakeStore" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>Fake Store</p></NavLink> */}
                 {/* <NavLink to="/MongoDB" className={(navData) => navData.isActive ? 'active-route' : 'inactive-route'}><p>MongoDB</p></NavLink> */}
